52209cd88031a4ce07ed86adeaf0dba8175eb591
nose15 <lukol12321@gmail.com>
2022-07-28 15:34:16 +0200

    Zmienilem baze danych

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Package, PackageStateTransitions
+from .models import Package, PackageStateTransitions, Route, Car
 
 # Register your models here.
 admin.site.register(Package)
 admin.site.register(PackageStateTransitions)
+admin.site.register(Route)
+admin.site.register(Car)
--- app/main/models.py
+++ app/main/models.py
@@ -3,17 +3,51 @@ from django.conf import settings
 import uuid
 
 registered = 'RE'
-in_preparation = 'PR'
-on_way = 'OW'
+en_route = 'ER'
 delivered = 'DE'
-suspended = 'SU'
+at_warehouse = 'AW'
 states = [
     (registered, 'Registered'),
-    (in_preparation, 'In Preparation'),
-    (on_way, 'On way'),
+    (en_route, 'En Route'),
     (delivered, 'Delivered'),
-    (suspended, 'Suspended'),
+    (at_warehouse, 'At warehouse'),
 ]
+
+
+class Route(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    origin = models.URLField()
+    destination = models.URLField()
+
+    state = models.CharField(
+        max_length=64
+    )
+
+    length = models.FloatField(default=0)
+
+
+class Car(models.Model):
+    registration_number = models.CharField(max_length=7, primary_key=True)
+    space = models.IntegerField()
+    filled = models.IntegerField()
+
+    driver_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to={'groups__name': "Drivers"},
+        related_name="Drivers",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None
+    )
+
+    route_id = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None,
+    )
 
 
 class Package(models.Model):
@@ -25,14 +59,42 @@ class Package(models.Model):
         default=registered
     )
 
-    customer_id = models.ForeignKey(
-        settings.AUTH_USER_MODEL, limit_choices_to={'groups__name': "Customers"},
-        related_name="Customers", on_delete=models.CASCADE
+    location = models.URLField()
+
+    sender_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to={'groups__name': "Customers"},
+        related_name="Senders",
+        on_delete=models.CASCADE,
+        null=True,
+        default=None
     )
 
-    driver_id = models.ForeignKey(
-        settings.AUTH_USER_MODEL, limit_choices_to={'groups__name': "Drivers"},
-        related_name="Drivers", on_delete=models.CASCADE
+    reciever_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        limit_choices_to={'groups__name': "Customers"},
+        related_name="Recievers",
+        on_delete=models.CASCADE,
+        null=True,
+        default=None
+    )
+
+    route_id = models.ForeignKey(
+        Route,
+        related_name="Routes",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None
+    )
+
+    car_id = models.ForeignKey(
+        Car,
+        related_name="Cars",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None
     )
 
 
@@ -44,3 +106,5 @@ class PackageStateTransitions(models.Model):
         default=registered
     )
     time = models.DateTimeField()
+
+
